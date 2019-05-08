b7bfbd89b7b83e23ac333d188c58407aace95205
Jasmine Chen <jasmine1226@gmail.com>
2019-05-08 06:15:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.size == 2
+  if array.size == 1
+    phrase = array[0]
+  elsif array.size == 2
     phrase = array.join(" and ")
   else
     last_word = array.pop
